ef25d7fbcdd9192931acdfba2d4a09d00888cae0
suraj singh <suraj.singh@shubpy.com>
2025-11-15 19:10:32 +0530

    setup

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+const Name_Test="suraj singh"
+
+const extracoom="3232";
+const extracoom2="3232";
+const extracoom3="3232";
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+   {Name_Test}
+    </div>
+  );
+}
